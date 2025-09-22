dbca13575e27c357b8bfa98cf4b4dac0e2af7dc2
Jessica Rehak <rehak@lbl.gov>
2025-09-22 13:30:32 -0700

    moved to qasync

--- ops/ecris/plotting/plotter_client.py
+++ ops/ecris/plotting/plotter_client.py
@@ -1,5 +1,6 @@
-from logging import getLogger
+import logging
 import asyncio
+import qasync
 import json
 import websockets
 from collections import deque
@@ -8,23 +9,19 @@ from PySide6 import QtWidgets
 
 import numpy as np
 
-_log = getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+_log = logging.getLogger(__name__)
 
 HISTORY_SIZE = 50
 MEAN_WINDOW_SIZE = 10
 
-app = QtWidgets.QApplication()
-plot = pg.plot(title='Real time data')
-plot.showGrid(x=True, y=True)
-data_line = plot.plot(pen=None, symbol='o', symbolSize=5)
-mean_line = plot.plot(pen='r')
 
 x_data = deque(maxlen=HISTORY_SIZE)
 y_data = deque(maxlen=HISTORY_SIZE)
 mean_plot_y = deque(maxlen=HISTORY_SIZE)
 mean_plot_x = deque(maxlen=HISTORY_SIZE)
 
-async def listen_and_plot():
+async def listen_and_plot(data_line, mean_line):
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
         _log.info(f"Connected to {uri}")
@@ -42,11 +39,29 @@ async def listen_and_plot():
                     mean_line.setData(mean_plot_x, mean_plot_y)
 
                 data_line.setData(x_data, y_data)
-                app.processEvents()
+                await asyncio.sleep(0)
+                
 
+def main():
+    app = QtWidgets.QApplication.instance() or QtWidgets.QApplication()
+    loop = qasync.QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
+    plot = pg.plot(title='Real time data')
+    plot.showGrid(x=True, y=True)
+    data_line = plot.plot(pen=None, symbol='o', symbolSize=5, name="Data")
+    mean_line = plot.plot(pen='r', name=f"Moving average (N={MEAN_WINDOW_SIZE})")
+    plot.addLegend()
+    with loop:
+        task = loop.create_task(listen_and_plot(data_line, mean_line))
+        loop.run_forever()
 
 if __name__ == "__main__":
     try:
-        asyncio.run(listen_and_plot())
+        main()
     except (KeyboardInterrupt, websockets.exceptions.ConnectionClosedError):
         _log.info("Shutting down plotter client.")
+    finally:
+        if 'loop' in locals() and loop.is_running():
+            loop.close()
+
