2fd902d45f12a9014dcc64c5450380df13bd407e
Jessica Rehak <rehak@lbl.gov>
2025-09-25 16:54:32 -0700

    refactored VENUS PLC tests to use parameterized tests

--- tests/test_venus_plc.py
+++ tests/test_venus_plc.py
@@ -5,32 +5,20 @@ from ops.ecris.devices.venus_plc import VenusPLC
 
 MODULE = 'ops.ecris.devices.venus_plc.'
 
-@pytest.fixture
-def mock_controller_and_plc():
+class TestVenusWriteData:
     mock_controller = MagicMock()
     plc = VenusPLC(mock_controller)
-    return mock_controller, plc
+    params = [
+        (VenusPLC.DataKeys.AVERAGE_CURRENT, 1.56E-6, 'fcv1_ammeter'),
+        (VenusPLC.DataKeys.CURRENT_STDEV, 3.67E-2, 'fcv1_ammeter_stdev')
+    ]
+    ids = ['average current', 
+           'current_stdev']
 
-
-@pytest.mark.asyncio
-async def test_venus_sends_correct_current(mock_controller_and_plc):
-    mock_controller, plc = mock_controller_and_plc
-    expected_current = 1.56E-6
-
-    await plc.write_data(VenusPLC.DataKeys.AVERAGE_CURRENT, 
-                         expected_current)
-    mock_controller.write.assert_called_once_with(
-        {'fcv1_ammeter': expected_current}
-    )
-
-@pytest.mark.asyncio
-async def test_venus_sends_correct_current_stdev(mock_controller_and_plc):
-    mock_controller, plc = mock_controller_and_plc
-    expected_current_stdev = 3.67E-2
-
-    await plc.write_data(VenusPLC.DataKeys.CURRENT_STDEV, 
-                         expected_current_stdev)
-    
-    mock_controller.write.assert_called_once_with(
-        {'fcv1_ammeter_stdev': expected_current_stdev}
-    )
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('key, value, plc_key', params, ids=ids)
+    async def test_write_data(self, key, value, plc_key):
+        self.mock_controller.reset_mock()
+        await self.plc.write_data(key, value)
+        self.mock_controller.write.assert_called_once_with({plc_key: value})
+        
