82504dc99361e6d01e319f70d0e111716ab40652
Jessica Rehak <rehak@lbl.gov>
2025-09-25 15:33:08 -0700

    updated labjack with disconnect method

--- ops/ecris/devices/labjack.py
+++ ops/ecris/devices/labjack.py
@@ -12,23 +12,38 @@ class LabJack(Device):
     def __init__(self) -> None:
         self._connection_lock = asyncio.Lock()
         self._handle: int | None = None
-        super().__init__()
+
+    @property
+    def is_connected(self) -> bool:
+        """Returns True if the device handle is open."""
+        return self._handle is not None
 
     async def connect(self) -> None:
+        """Opens a connection to the LabJack device."""
         async with self._connection_lock:
             if self.is_connected:
                 _log.debug('LabJack is already connected')
                 return
+            
+            _log.info("Connecting to LabJack T8...")
             self._handle = await asyncio.to_thread(ljm.openS, "T8", "usb", "ANY")
-            return
+            _log.info("LabJack connected.")
 
-    async def get_data(self, data_key: Any) -> float:
-        return await super().get_data(data_key)
+    async def disconnect(self) -> None:
+        """Closes the connection to the LabJack device."""
+        async with self._connection_lock:
+            if not self.is_connected:
+                _log.debug("LabJack is already disconnected.")
+                return
+
+            _log.info("Disconnecting from LabJack...")
+            await asyncio.to_thread(ljm.close, self._handle)
+            self._handle = None
+            _log.info("LabJack disconnected.")
 
     async def write_data(self, data_key: Any, value: float) -> None:
-        return await super().write_data(data_key, value)
+        pass
 
+    async def get_data(self, data_key: Any) -> float:
+        pass
 
-    @property
-    def is_connected(self) -> bool:
-        return self._handle is not None
