886a310fedda6bd9dac6c7214f68f2b3514a256e
Jessica Rehak <rehak@lbl.gov>
2025-09-26 10:55:34 -0700

    updated file io to be async, updated test

--- ops/ecris/tasks/csd.py
+++ ops/ecris/tasks/csd.py
@@ -1,10 +1,11 @@
+import aiofiles
 from pathlib import Path
 
 from ops.ecris.devices.venus_plc import VenusPLC
 
 async def write_datasheet(output_path: Path, venus_plc: VenusPLC) -> None:
      all_data = await venus_plc.get_all_data()
-     with open(output_path, 'w') as f:
+     async with aiofiles.open(output_path, 'w') as f:
           for idx, data in all_data.items():
                data_name, data_value = data
-               f.write(f'{idx:>4} {data_value:.5e} {data_name}\n')
+               await f.write(f'{idx:>4} {data_value:.5e} {data_name}\n')
--- tests/test_csd_characterization.py
+++ tests/test_csd_characterization.py
@@ -1,6 +1,6 @@
 import pytest
 from pathlib import Path
-from unittest.mock import patch, MagicMock, mock_open, call
+from unittest.mock import patch, MagicMock, mock_open, call, AsyncMock
 import time
 
 from ops.ecris.devices.venus_plc import VenusPLC
@@ -11,6 +11,18 @@ from ops.ecris.tasks.csd import write_datasheet
 LEGACY_MODULE = 'tests.legacy_code.legacy_functions.'
 VENUS_MODULE = 'ops.ecris.devices.venus_plc.'
 MODULE = 'ops.ecris.tasks.csd.'
+
+
+class MockAsyncFile:
+    file_handler = AsyncMock()
+    async def write(self):
+        pass
+
+    async def __aenter__(self):
+        return self.file_handler
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
 
 class TestDataSheet:
     VAR_NAMES = ['extraction_v', 'fcv1_ammeter', 'batman_i_set']
@@ -46,20 +58,20 @@ class TestDataSheet:
 
     @pytest.mark.asyncio
     async def test_datasheet_characterization(self):
-        mock_controller = MagicMock()
-        mock_controller.read_vars.return_value = self.VAR_NAMES
-        mock_controller.read.side_effect = self.VAR_VALUES
-        venus_plc = VenusPLC(mock_controller)
-        path = Path(self.EXPECTED_FILEPATH)
+        mock_all_data = {i: (self.VAR_NAMES[i], self.VAR_VALUES[i])
+                         for i in range(len(self.VAR_NAMES))}
 
-        with patch(VENUS_MODULE + 'VENUSController', mock_controller), \
-             patch(MODULE + 'open', mock_open()) as mocked_file:
-            await write_datasheet(path, venus_plc)
+        mock_venus_plc = AsyncMock(spec=VenusPLC)
+        mock_venus_plc.get_all_data.return_value = mock_all_data
         
-        mocked_file.assert_called_once_with(path, 'w')
-        mock_controller.read_vars.assert_called_once_with()
-        expected_read_calls = [call([name]) for name in self.VAR_NAMES]
+        path = Path(self.EXPECTED_FILEPATH)
+        mock_async_file = MockAsyncFile()
 
-        assert mock_controller.read.call_args_list == expected_read_calls
-        handle = mocked_file()
-        assert handle.write.call_args_list == self.EXPECTED_WRITES
+        with patch('aiofiles.open', return_value=mock_async_file) as mock_open:
+            await write_datasheet(path, mock_venus_plc)
+        
+        mock_open.assert_called_once_with(path, 'w')
+        mock_venus_plc.get_all_data.assert_awaited_once_with()
+
+        handler = mock_async_file.file_handler 
+        assert handler.write.await_args_list == self.EXPECTED_WRITES
