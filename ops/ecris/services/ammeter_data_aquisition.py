c461770b5bfaf97f3b90384d40bedca513d16c80
Jessica Rehak <rehak@lbl.gov>
2025-10-02 10:11:16 -0700

    corrected name of base acquisition, updated logging

--- ops/ecris/services/ammeter_data_aquisition.py
+++ ops/ecris/services/ammeter_data_aquisition.py
@@ -1,9 +1,9 @@
 from ops.ecris.devices import Ammeter
 from ops.ecris.model.measurement import Measurement
-from .base_aquisition import TelnetDataAquisitionService
+from .base_aquisition import TelnetDataAcquisitionService
 from ops.ecris.operations.producers import time_average_current
 
-class AverageCurrentAquisitionService(TelnetDataAquisitionService):
+class AverageCurrentAquisitionService(TelnetDataAcquisitionService):
     def __init__(self, ammeter: Ammeter) -> None:
         self.device: Ammeter
         super().__init__(ammeter)
--- ops/ecris/services/base_aquisition.py
+++ ops/ecris/services/base_aquisition.py
@@ -9,41 +9,41 @@ from ops.ecris.model.measurement import Measurement
 
 _log = getLogger(__name__)
 
-class TelnetDataAquisitionService(abc.ABC):
+class TelnetDataAcquisitionService(abc.ABC):
     def __init__(self, device: TelnetDevice) -> None:
         self.device = device
         self._loop: asyncio.AbstractEventLoop | None = None
         self._data_queue: asyncio.Queue | None = None
 
     @abc.abstractmethod
-    def _aquire_data(self) -> Measurement:
+    def _acquire_data(self) -> Measurement:
         raise NotImplementedError
 
-    async def start(self, aquisition_rate: float) -> None:
-        _log.info(f'Starting data acquisition service for "{self.device.id}"...')
+    async def start(self, acquisition_rate: float) -> None:
+        _log.info(f'Starting {self.__class__.__name__} for "{self.device.id}"...')
         self._loop = asyncio.get_running_loop()
-        self._data_queue = asyncio.Queue()
+        
         await self.device.connect()
+
         self._producer_thread = threading.Thread(
             target=producer_thread,
-            args=(self._loop, self._data_queue, self._aquire_data, aquisition_rate),
-            daemon=True)
+            args=(self._loop, self._data_queue, self._acquire_data, acquisition_rate),
+            daemon=True,
+            name=f"{self.device.id}_Producer"
+        )
         self._producer_thread.start()
         self._is_running = True
-        _log.debug('Data aquisition service started.')
+        _log.debug(f'{self.__class__.__name__} started successfully.')
 
     @property
     def data_queue(self) -> asyncio.Queue | None:
         return self._data_queue
 
     async def stop(self):
-        """Stops the service and disconnects from the device."""
         if not self._is_running:
             return
-        _log.info(f'Stopping data aquisition for "{self.device.id}"...')
-        
+        _log.info(f'Stopping {self.__class__.__name__} for "{self.device.id}"...')
         if self.device.is_connected:
             _log.debug(f'Disconnecting device "{self.device.id}"')
             await self.device.disconnect()
-        
         self._is_running = False
