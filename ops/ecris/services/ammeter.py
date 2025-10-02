72681747a6fd17d24333c5470c64c2dc67eb5e72
Jessica Rehak <rehak@lbl.gov>
2025-10-02 12:15:57 -0700

    coupled distributer startup to ammeter current service

--- ops/ecris/services/ammeter.py
+++ ops/ecris/services/ammeter.py
@@ -1,4 +1,5 @@
 import asyncio
+from logging import getLogger
 import time
 from ops.ecris.devices import Ammeter
 from ops.ecris.model.measurement import ValueMeasurement
@@ -6,22 +7,33 @@ from .base_acquisition import TelnetDataAcquisitionService
 from .distributor import DataDistributor
 from .processors import AveragingProcessor
 
+_log = getLogger(__name__)
+
 class CurrentAcquisitionService(TelnetDataAcquisitionService):
     def __init__(self, ammeter: Ammeter):
         super().__init__(ammeter)
-        # It creates and owns the distributor. The distributor is fed by the base class's _data_queue.
+        # Distributer for current data
         self.distributor = DataDistributor(self._data_queue)
+        self._distributor_task: asyncio.Task | None = None
 
     async def start(self) -> None:
         await super().start()
+        self._distributor_task = asyncio.create_task(self.distributor.run())
+
+    async def stop(self):
+        if self._distributor_task and not self._distributor_task.done():
+            self._distributor_task.cancel()
+            await asyncio.gather(self._distributor_task, return_exceptions=True)
+        await super().stop()
 
     def _acquire_data(self) -> ValueMeasurement:
         coroutine = self.device.read_data(Ammeter.DataKeys.CURRENT)
         future = asyncio.run_coroutine_threadsafe(coroutine, self._loop)
+        data = future.result()
         return ValueMeasurement(
             source=self.device.id,
             timestamp=time.time(),
-            value=future.result()
+            value=data
         )
 
 class AverageCurrentService:
