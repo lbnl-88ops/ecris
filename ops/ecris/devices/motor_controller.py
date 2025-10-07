720168ba707fbdac8d5050c61afc84097d8bd964
Jessica Rehak <rehak@lbl.gov>
2025-10-07 15:14:26 -0700

    implemented motor controller setup

--- ops/ecris/devices/motor_controller.py
+++ ops/ecris/devices/motor_controller.py
@@ -1,4 +1,5 @@
 import asyncio
+from enum import StrEnum
 from logging import getLogger
 from typing import Any
 
@@ -7,6 +8,11 @@ from ops.ecris.model.device import TelnetDevice
 _log = getLogger(__name__)
 
 class MotorController(TelnetDevice):
+
+    class Commands(StrEnum):
+        OPEN_PROGRAM0 = 'PROG0'
+        SET_RAMPING = "ACC 5 DEC 5 VEL 15 STP 100"
+
     def __init__(self, id: str = 'ACR74C', 
                  ip: str | None = None, 
                  port: int | None = None, 
@@ -19,3 +25,22 @@ class MotorController(TelnetDevice):
 
     async def write_data(self, data_key: Any, value: float) -> None:
         raise NotImplementedError
+
+    async def connect(self) -> None:
+        _log.debug(f'Connecting Motor Controller at {self._host}')
+        await super().connect()
+        await self._setup()
+
+    async def send_command(self, command: str):
+        await self._write(command)
+        raw_response = await self._read_until(self._prompt if self._prompt is not None else '\n')
+        response_lines = [l.strip() for l in raw_response.split()]
+        response = [l for l in response_lines if l != self._prompt and l != command]
+        if len(response) == 1:
+            return response[0]
+        return response
+
+    async def _setup(self) -> None:
+        self._prompt = 'P00>'
+        await self.send_command(MotorController.Commands.OPEN_PROGRAM0)
+        await self.send_command(MotorController.Commands.SET_RAMPING)
--- tests/devices/test_motor_controller.py
+++ tests/devices/test_motor_controller.py
@@ -20,13 +20,24 @@ def mock_motor_controller_connection():
 @pytest.mark.asyncio
 async def test_connect_sends_correct_commands(mock_motor_controller_connection):
     controller, mock_reader, mock_writer, mock_open_conn = mock_motor_controller_connection
-    banner = 'Unknown values.\r\nSYS>'
 
-    side_effects = [banner.encode('ascii')]
+    # 1. Test values
+    banner = 'Unknown values.\r\nSYS>'
+    commands = ["PROG0", "ACC 5 DEC 5 VEL 15 STP 100"]
+    side_effects = [banner.encode('ascii')] + [f'{s}\r\nP00>'.encode('ascii') for s in commands]
+    expected_write_calls = [call(f'{s}\r\n'.encode('ascii')) for s in commands]
+    expected_readuntil_calls = [call(s.encode('ascii')) 
+                                for s in ['SYS>'] + ['P00>'] * len(commands)]
+
+    # 2. Set up mocks
     mock_reader.readuntil.side_effect = side_effects
 
+    # 3. Run command
     await controller.connect()
 
+    # 4. Test assertions
     mock_open_conn.assert_awaited_once_with('127.0.0.1', 9999, encoding=False)
-    mock_reader.readuntil.assert_called_once_with('SYS>'.encode('ascii'))
-    assert mock_reader.readuntil.call_count == len(side_effects)
+    mock_reader.readuntil.assert_has_calls(expected_readuntil_calls)
+    mock_writer.write.assert_has_calls(expected_write_calls)
+    assert mock_reader.readuntil.call_count == len(expected_readuntil_calls)
+    assert mock_writer.write.call_count == len(expected_write_calls)
